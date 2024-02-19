20fc80f5a3b8c39fa6038ada948c55446f30e641
camolezi <lucascamolezi@gmail.com>
2024-02-18 22:03:11 -0300

    feat: save disassembled file in txt file

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,22 @@ use chip8_webgpu::{
         decoder::bit_manipulation::group_in_raw_instructions,
         disassembler::disassemble::disassemble_raw_instructions,
     },
-    utilities::storage,
+    utilities::storage::{self, write_disassembled_to_file},
 };
+use inquire::Select;
 
 fn main() {
+    let execution_options: Vec<&str> = vec!["Disassemble", "Exit"];
+    let selected_execution = Select::new("", execution_options)
+        .prompt()
+        .expect("Error reading user execution option");
+
     let rom_data = storage::read_rom("ibm_logo").expect("Unable to load rom");
 
     let instructions = group_in_raw_instructions(&rom_data);
 
     let disassembled_instructions = disassemble_raw_instructions(&instructions);
 
-    for instruction in disassembled_instructions {
-        println!("{:?}", instruction);
-    }
+    write_disassembled_to_file("ibm_logo", &disassembled_instructions)
+        .expect("was not able to write file")
 }
--- src/utilities/storage.rs
+++ src/utilities/storage.rs
@@ -7,3 +7,19 @@ pub fn read_rom(rom_name: &str) -> Result<Vec<u8>, std::io::Error> {
     println!("Reading ROM at {:#?}", rom_path);
     std::fs::read(rom_path)
 }
+
+const DISASSEMBLED_DIR_PATH: &'static str = "disassembled";
+
+pub fn write_disassembled_to_file(
+    file_name: &str,
+    disassembled_instructions: &Vec<String>,
+) -> Result<(), std::io::Error> {
+    let disassembled_path = Path::new(DISASSEMBLED_DIR_PATH).join(file_name.to_owned() + ".txt");
+    println!("Writing disassembled_path  rom at {:#?}", disassembled_path);
+
+    let file_content = disassembled_instructions
+        .iter()
+        .fold(String::new(), |acc, x| acc + x + "\n");
+
+    std::fs::write(disassembled_path, file_content)
+}
